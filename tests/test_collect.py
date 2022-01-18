e2c9b4c6e4989531a0fc825cd36561714c6ab541
Hercules <hercosmith03@gmail.com>
2022-01-18 12:30:06 +0200

    Tests for collect stages

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -1,2 +1,39 @@
-def test_collect():
-    raise NotImplementedError()
+import diskcache
+import pytest
+from micropipe import CollectDeque, CollectList
+from micropipe.types import EndFlow, FlowValue
+
+
+def checkEqual(l1: list, l2: list):
+    return len(l1) == len(l2) and sorted(l1) == sorted(l2)
+
+
+@pytest.mark.asyncio
+async def test_collect_list():
+    stage = CollectList()
+    for i in range(10):
+        stage._input_queue.put_nowait(FlowValue(i))
+    stage._input_queue.put_nowait(EndFlow())
+
+    await stage._flow()
+
+    assert stage._output_queue.qsize() == 2
+    assert stage._output_queue.get_nowait() == FlowValue([i for i in range(10)])
+    assert stage._output_queue.get_nowait() == EndFlow()
+
+
+@pytest.mark.asyncio
+async def test_collect_deque():
+    stage = CollectDeque()
+    for i in range(10):
+        stage._input_queue.put_nowait(FlowValue(i))
+    stage._input_queue.put_nowait(EndFlow())
+
+    await stage._flow()
+
+    assert stage._output_queue.qsize() == 2
+    val = stage._output_queue.get_nowait()
+    assert isinstance(val.value, diskcache.Deque)
+    assert len(val.value) == 10
+    assert val.value.peek() == 9
+    assert val.value.peekleft() == 0
