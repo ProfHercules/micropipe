5a7fe920fc9f64a6e3e3e9eadbb342d7c20aa19b
Hercules <hercosmith03@gmail.com>
2022-01-18 16:36:12 +0200

    Add PassthroughStage tests

--- micropipe/stage/passthrough.py
+++ micropipe/stage/passthrough.py
@@ -1,3 +1,5 @@
+from copy import copy, deepcopy
+from enum import Enum
 from typing import Callable, Generic, TypeVar
 
 from micropipe.stage.base import BaseStage
@@ -6,23 +8,35 @@ from micropipe.types import FlowValue
 I = TypeVar("I")  # input
 
 
+class CopyMode(Enum):
+    SHALLOW = 1
+    DEEP = 2
+    NONE = 3
+
+
 class PassthroughStage(Generic[I], BaseStage[I, I]):
     __func: Callable[[FlowValue[I]], None]
+    __copy_mode: CopyMode
 
     def __init__(
         self,
         func: Callable[[FlowValue[I]], None],
+        copy_mode: CopyMode = CopyMode.NONE,
         **kwargs,
     ):
         super(PassthroughStage, self).__init__(**kwargs)
         self.__func = func
+        self.__copy_mode = copy_mode
 
     async def _task_handler(self, flow_val: FlowValue[I]) -> bool:
         result = self._wrap_flow_value(flow_val.value, flow_val.meta)
         await self._output_queue.put(result)
 
-        hash_before = hash(flow_val)
-        self.__func(flow_val)
-        assert hash_before == hash(flow_val)
+        if self.__copy_mode is CopyMode.NONE:
+            self.__func(flow_val)
+        elif self.__copy_mode is CopyMode.SHALLOW:
+            self.__func(copy(flow_val))
+        elif self.__copy_mode is CopyMode.DEEP:
+            self.__func(deepcopy(flow_val))
 
         return True
--- tests/test_passthrough.py
+++ tests/test_passthrough.py
@@ -1,2 +1,92 @@
-def test_passthrough():
-    raise NotImplementedError()
+from typing import Dict
+
+import pytest
+
+from micropipe import EndFlow, FlowValue, PassthroughStage
+from micropipe.stage.passthrough import CopyMode
+
+
+# CopyMode.NONE means the object can be modified by Passthrough func
+@pytest.mark.asyncio
+async def test_passthrough_no_copy_modify():
+    stage = PassthroughStage[Dict[str, str]](
+        lambda x: x.value.__setitem__("foo", "bar"),
+        copy_mode=CopyMode.NONE,
+    )
+
+    stage._input_queue.put_nowait(FlowValue({}))
+    stage._input_queue.put_nowait(EndFlow())
+
+    assert stage._input_queue.qsize() == 2
+    await stage._flow()
+    assert stage._output_queue.qsize() == 2
+
+    mockval = stage._output_queue.get_nowait().value
+    # object was modified by passthrough
+    assert mockval.get("foo") == "bar"
+
+
+# CopyMode.SHALLOW means basic attrs cannot be modified
+@pytest.mark.asyncio
+async def test_passthrough_shallow_copy_shallow_modify():
+    original, modified = {"foo": "bar"}, {"foo": "baz"}
+
+    stage = PassthroughStage[Dict[str, str]](
+        lambda x: x.__setattr__("value", modified),
+        copy_mode=CopyMode.SHALLOW,
+    )
+
+    stage._input_queue.put_nowait(FlowValue(original))
+    stage._input_queue.put_nowait(EndFlow())
+
+    assert stage._input_queue.qsize() == 2
+    await stage._flow()
+    assert stage._output_queue.qsize() == 2
+
+    mockval = stage._output_queue.get_nowait().value
+    # object was not modified by passthrough
+    assert mockval.get("foo") == original.get("foo")
+
+
+# CopyMode.SHALLOW means child references on the object can be modified by Passthrough func
+@pytest.mark.asyncio
+async def test_passthrough_shallow_copy_deep_modify():
+    original = {"foo": {}}
+
+    stage = PassthroughStage[Dict[str, dict]](
+        lambda x: x.value["foo"].__setitem__("bar", "baz"),
+        copy_mode=CopyMode.SHALLOW,
+    )
+
+    stage._input_queue.put_nowait(FlowValue(original))
+    stage._input_queue.put_nowait(EndFlow())
+
+    assert stage._input_queue.qsize() == 2
+    await stage._flow()
+    assert stage._output_queue.qsize() == 2
+
+    mockval = stage._output_queue.get_nowait().value
+    # object was modified by passthrough
+    assert mockval.get("foo") == {"bar": "baz"}
+
+
+# CopyMode.DEEP means the object is safe from modifications
+@pytest.mark.asyncio
+async def test_passthrough_deep_copy_deep_modify():
+    original = {"foo": {}}
+
+    stage = PassthroughStage[Dict[str, dict]](
+        lambda x: x.value["foo"].__setitem__("bar", "baz"),
+        copy_mode=CopyMode.DEEP,
+    )
+
+    stage._input_queue.put_nowait(FlowValue(original))
+    stage._input_queue.put_nowait(EndFlow())
+
+    assert stage._input_queue.qsize() == 2
+    await stage._flow()
+    assert stage._output_queue.qsize() == 2
+
+    mockval = stage._output_queue.get_nowait().value
+    # object was modified by passthrough
+    assert mockval.get("foo") == {}
